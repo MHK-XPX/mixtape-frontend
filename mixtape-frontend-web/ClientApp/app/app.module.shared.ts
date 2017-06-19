ceae739ee3cf08b971410063c487c6246da1f5df
Evan Lindsey <elindsey@xpanxion.com>
2017-06-19 15:03:05 -0500

    seed repo with angular spa template

---
+++ mixtape-frontend-web/ClientApp/app/app.module.shared.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component'
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+// Add Page Components Here
+
+export const sharedConfig: NgModule = {
+    bootstrap: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent
+    ],
+    imports: [
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: '**', redirectTo: 'home' }
+            // Add Router Paths to Components Here
+        ])
+    ]
+};
