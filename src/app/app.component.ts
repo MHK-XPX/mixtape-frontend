5ac6803b97f682516aa0f311f64471afc3f649da
Ryan Kruse <rpkruse@ksu.edu>
2017-07-07 12:49:57 -0500

    Made the UI look better
    Have a hover over dropdown menu rather than on click, flows much better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import {Component} from '@angular/core';
-
+import { UserService } from './user/user.service';
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
+  constructor(private _userService: UserService){}
+
   showDropDown(): void{
     document.getElementById("dropDown").classList.toggle("show");
+  }
+
+  logOut(){
+    this._userService.logOut();
   }
 }
