1858dc25bb68bef50c5fdc9e6b1e878129cc2f10
Ryan Kruse <rpkruse@ksu.edu>
2017-12-21 09:23:13 -0600

    Removed Angular 2 framework (and files associated with it)
    Removed any angular 2 files so we can get ready for the Angular 5 CLI update

--- src/app/playlist/interfaces/playlist.ts
+++
@@ -1,9 +0,0 @@
-import { PlaylistSong } from './playlistsong';
-
-export interface PlayList{
-    playlistId: number;
-    active: boolean;
-    name: string;
-    userId: number;
-    playlistSong: PlaylistSong[];
-}
